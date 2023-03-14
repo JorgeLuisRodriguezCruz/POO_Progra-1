6a7939d35600a842e37f3f8c768b09289a59ee20
JorgeLuisRodriguezCruz <rodriguezcruz088@gmail.com>
2023-03-13 22:00:34 -0600

    Se agrega configuraciones UI

--- POO_Proyecto_1/src/main/java/controller/Main.java
+++ POO_Proyecto_1/src/main/java/controller/Main.java
@@ -1,12 +1,14 @@
 package controller;
 
 import view.Mapa;
+import view.MicrogamerGUI;
 
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Qué pasa? Algun problema?");
         
-        Mapa vent = new Mapa ();
+        //Mapa vent = new Mapa ();
+        MicrogamerGUI uimicro = new MicrogamerGUI();
     }
 }
--- POO_Proyecto_1/src/main/java/view/MicrogamerGUI.java
+++ POO_Proyecto_1/src/main/java/view/MicrogamerGUI.java
@@ -1,86 +1,97 @@
-import javax.swing.*;
-import java.awt.*;
+package view;
+
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
 
-    public class MicrogamerGUI {
+public class MicrogamerGUI {
 
-        public MicrogamerGUI(){
-            // Crear la ventana principal
-            JFrame ventanaPrincipal = new JFrame("MicroGamer                                           Versión 1.1");
-           ventanaPrincipal.setSize(600, 680);
-           ventanaPrincipal.setLocationRelativeTo(null);
-           ventanaPrincipal.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-           ventanaPrincipal.getContentPane().setBackground(Color.lightGray);
-           ventanaPrincipal.setResizable(false);
-
-
-    
-            // Crear la etiqueta de título
-
-            JLabel labelTitulo = new JLabel("MicroGamer", SwingConstants.CENTER);
-            labelTitulo.setFont(new Font("Forte", Font.BOLD, 48));
-            labelTitulo.setForeground(Color.BLACK);
-            labelTitulo.setBounds(100, 20, 420, 50);
-
-    
-            // Crear las etiquetas de saludo y escáner
-            JLabel labelMaxAtributos = new JLabel("-Máxima capacidad de los atributos", SwingConstants.CENTER);
-            labelMaxAtributos.setFont(new Font("Comic Sans Ms", Font.BOLD, 18));
-            labelMaxAtributos.setBounds(1, 80, 400, 30);
-    
-            JTextField MaxAtributos = new JTextField();
-            MaxAtributos.setBounds(380, 80, 150, 30);
-    
-            JLabel labelMinAtributos = new JLabel("-Mínima capacidad de los atributos  ", SwingConstants.CENTER);
-            labelMinAtributos.setFont(new Font("Comic Sans Ms", Font.BOLD, 18));
-            labelMinAtributos.setBounds(1, 120, 400, 30);
-    
-            JTextField MinAtributos = new JTextField();
-            MinAtributos.setBounds(380, 120, 150, 30);
-    
-            JLabel labelAumento = new JLabel("-Aumento de los atributos                 ", SwingConstants.CENTER);
-            labelAumento.setFont(new Font("Comic Sans Ms", Font.BOLD, 18));
-            labelAumento.setBounds(20, 160, 400, 30);
-    
-            JTextField AumentoAtributos = new JTextField();
-            AumentoAtributos.setBounds(380, 160, 150, 30);
-    
-            JLabel labelDecremento = new JLabel("-Decremento de los atributos            ", SwingConstants.CENTER);
-            labelDecremento.setFont(new Font("Comic Sans Ms", Font.BOLD, 18));
-            labelDecremento.setBounds(15, 200, 400, 30);
-    
-            JTextField DecrementoAtributos = new JTextField();
-            DecrementoAtributos.setBounds(380, 200, 150, 30);
-    
-            // Crear el botón de jugar partida
+    public MicrogamerGUI(){
+        // Crear la ventana principal
+        JFrame ventanaPrincipal = new JFrame("MicroGamer                                           Versión 1.1");
+       ventanaPrincipal.setSize(600, 680);
+       ventanaPrincipal.setLocationRelativeTo(null);
+       ventanaPrincipal.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       ventanaPrincipal.getContentPane().setBackground(Color.lightGray);
+       ventanaPrincipal.setResizable(false);
 
 
-            JButton botonJugar = new JButton("JUGAR");
-            botonJugar.setFont(new Font("Comic Sans Ms", Font.BOLD, 28));
-            botonJugar.setBounds(80, 270, 200, 40);
-            botonJugar.setBackground(Color.GREEN);
 
-            botonJugar.addActionListener(e ->{
-                try {
-                    int valorMAX = Integer.parseInt(MaxAtributos.getText());
-                    
+        // Crear la etiqueta de título
 
-                    int valorMIN = Integer.parseInt(MinAtributos.getText());
-                    
+        JLabel labelTitulo = new JLabel("MicroGamer", SwingConstants.CENTER);
+        labelTitulo.setFont(new Font("Forte", Font.BOLD, 48));
+        labelTitulo.setForeground(Color.BLACK);
+        labelTitulo.setBounds(100, 20, 420, 50);
 
-                    int valorAUMENTO = Integer.parseInt(AumentoAtributos.getText());
-                    
 
-                    int valorDECREMENTO = Integer.parseInt(DecrementoAtributos.getText());
-                    
-                  } 
-                  catch (NumberFormatException ex) {
-                    JOptionPane.showMessageDialog(null, "ERROR: Los valores digitados deben de ser enteros. Digite nuevamente los valores.");
-                  }
-                });
+        // Crear las etiquetas de saludo y escáner
+        JLabel labelMaxAtributos = new JLabel("-Máxima capacidad de los atributos", SwingConstants.CENTER);
+        labelMaxAtributos.setFont(new Font("Comic Sans Ms", Font.BOLD, 18));
+        labelMaxAtributos.setBounds(1, 80, 400, 30);
+
+        JTextField MaxAtributos = new JTextField();
+        MaxAtributos.setBounds(380, 80, 150, 30);
+
+        JLabel labelMinAtributos = new JLabel("-Mínima capacidad de los atributos  ", SwingConstants.CENTER);
+        labelMinAtributos.setFont(new Font("Comic Sans Ms", Font.BOLD, 18));
+        labelMinAtributos.setBounds(1, 120, 400, 30);
+
+        JTextField MinAtributos = new JTextField();
+        MinAtributos.setBounds(380, 120, 150, 30);
+
+        JLabel labelAumento = new JLabel("-Aumento de los atributos                 ", SwingConstants.CENTER);
+        labelAumento.setFont(new Font("Comic Sans Ms", Font.BOLD, 18));
+        labelAumento.setBounds(20, 160, 400, 30);
+
+        JTextField AumentoAtributos = new JTextField();
+        AumentoAtributos.setBounds(380, 160, 150, 30);
+
+        JLabel labelDecremento = new JLabel("-Decremento de los atributos            ", SwingConstants.CENTER);
+        labelDecremento.setFont(new Font("Comic Sans Ms", Font.BOLD, 18));
+        labelDecremento.setBounds(15, 200, 400, 30);
+
+        JTextField DecrementoAtributos = new JTextField();
+        DecrementoAtributos.setBounds(380, 200, 150, 30);
+
+        // Crear el botón de jugar partida
+
+
+        JButton botonJugar = new JButton("JUGAR");
+        botonJugar.setFont(new Font("Comic Sans Ms", Font.BOLD, 28));
+        botonJugar.setBounds(80, 270, 200, 40);
+        botonJugar.setBackground(Color.GREEN);
+
+        botonJugar.addActionListener(e ->{
+            try {
+                int valorMAX = Integer.parseInt(MaxAtributos.getText());
+
+
+                int valorMIN = Integer.parseInt(MinAtributos.getText());
+
+
+                int valorAUMENTO = Integer.parseInt(AumentoAtributos.getText());
+
+
+                int valorDECREMENTO = Integer.parseInt(DecrementoAtributos.getText());
+
+              } 
+              catch (NumberFormatException ex) {
+                JOptionPane.showMessageDialog(null, "ERROR: Los valores digitados deben de ser enteros. Digite nuevamente los valores.");
+              }
+        }
+    );
 
 
 
@@ -114,7 +125,7 @@ import javax.swing.JOptionPane;
 
     
             // Agregar los componentes a la ventana
-        ventanaPrincipal.setLayout(null);
+           ventanaPrincipal.setLayout(null);
            ventanaPrincipal.add(labelTitulo);
            ventanaPrincipal.add(labelMaxAtributos);
            ventanaPrincipal.add(MaxAtributos);
