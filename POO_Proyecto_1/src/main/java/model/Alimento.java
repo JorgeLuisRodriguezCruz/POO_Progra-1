b260deb5a0affdadacbc51efe3c11bd85e540e5e
JorgeLuisRodriguezCruz <rodriguezcruz088@gmail.com>
2023-03-16 11:49:08 -0600

    Se agregan super clases alimento y organismo

---
+++ POO_Proyecto_1/src/main/java/model/Alimento.java
@@ -0,0 +1,14 @@
+package model;
+
+public class Alimento {
+    private int cordenadad[];
+    private int cantidad;
+    private int turnosParaReaparecer;
+
+    public Alimento() {
+    }
+    
+    public void serComido (Organismo org) { 
+    }
+    
+}
---
+++ POO_Proyecto_1/src/main/java/model/Organismo.java
@@ -0,0 +1,21 @@
+package model;
+
+public class Organismo {
+    private int cordenadas[];
+    private int edad;
+    private int vision;
+    private int energia;
+    private int velocidad;
+
+    public Organismo() {
+    }
+    
+    public int elegirDireccion () {
+        return 0;
+    }
+    
+    public void moverse () {
+        
+    }
+    
+}
